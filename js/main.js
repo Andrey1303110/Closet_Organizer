35db56dca63afefcb8c8c82504049d3ea9a2892c
andrey1303110 <andrey1303110@gmail.com>
2022-06-27 16:28:33 +0300

    fix phaser main settings in config

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1080,
+    width: 575,
     height: 1080,
     backgroundColor: '#CCC9C9',
     orientation: Phaser.Scale.PORTRAIT, 
     scale: {
-        mode: Phaser.Scale.ENVELOP,
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
       },
     scene: [Preloader, GameScene]
