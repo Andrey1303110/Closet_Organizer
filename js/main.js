c72f3705935ee604b19ed123d23d471e0df51a02
andrey1303110 <andrey1303110@gmail.com>
2022-06-28 16:22:29 +0300

    fix config for responsive, add values to config

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
-    width: 575,
+    width: 1080,
     height: 1080,
     backgroundColor: '#CCC9C9',
     orientation: Phaser.Scale.PORTRAIT, 
     scale: {
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.ENVELOP,
         autoCenter: Phaser.Scale.CENTER_BOTH,
       },
-    scene: [Preloader, GameScene]
+    scene: [Preloader, GameScene],
+    hamper: {
+      width: 143,
+      height: 210,
+    },
+    hamperNames: ['dress', 'underpants', 'underwear'],
+    clothingSettings: { 
+      dress: {
+        nums: 12
+      },
+      underwear: {
+        nums: 18,
+        lines: 3,
+      },
+      underpants: {
+        nums: 32,
+        lines: 2
+      },
+    },
+    progresStages: {
+			1: .5,
+			2: .8,
+			3: 1,
+		},
 };
 
 var game = new Phaser.Game(config);
